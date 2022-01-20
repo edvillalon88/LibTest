28f4dd5e5a988467c2c442046216ef516d1fff0b
Eduardo Valdes <edvillalon88@gmail.com>
2022-01-20 13:52:35 -0600

    first commit

---
+++ components/Button/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+export interface TestButtonInterface {
+    label: string;
+}
+declare const Button: ({ label }: TestButtonInterface) => JSX.Element;
+export default Button;
